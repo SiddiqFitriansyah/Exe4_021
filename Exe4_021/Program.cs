24d5781bd7da1175673d3b9f3b4e116e823ca086
SiddiqFitriansyah <siddiqfitriansyah14@gmail.com>
2022-12-09 17:21:37 +0700

    Finish the Project

--- Exe4_021/Program.cs
+++ Exe4_021/Program.cs
@@ -6,63 +6,109 @@ namespace Exe4_021
     {
         public int info;
         public Siddiq next;
-        private int[] a = new int[41];
-        public Siddiq(int i, Siddiq S)
+        public Siddiq(int i, Siddiq SF)
         {
-            info = i;
-            next = null;
+            info = i;   
+            next = SF;  
         }
     }
     class Stack
     {
-        private int[] ele;
+        private int[] Siddiq;
         private int top;
         private int max;
         public Stack(int size)
         {
-            ele = new int[size];
+            Siddiq = new int[size];
             top = -1;
             max = size;
         }
+        bool empty()
+        {
+            if (top == null)
+                return (true);
+            else
+                return (false);
+        }
         public void Push(int item)
         {
-            if (top == max-1)
+            if (top == max -1)
             {
                 Console.WriteLine("Stack Overflow");
                 return;
             }
             else
             {
-                ele[++top] = item;
+                Siddiq[++top] = item;
             }
         }
         public int Pop()
         {
-            if(top == -1)
+            if (top == -1)
             {
                 Console.WriteLine("Stack Underflow");
                 return -1;
             }
             else
             {
-                Console.WriteLine(" Poped Element is: " + ele[top]);
-                return ele[top--];
+                Console.WriteLine(" Poped Element is: " + Siddiq[top]);
+                return Siddiq[top--];
             }
         }
         public void Display()
         {
-            if (top == -1)
-            {
-                Console.WriteLine("Stack is Empty");
-                return;
-            }
+            Siddiq tmp;
+            if (empty())
+                Console.WriteLine("\nStack is Empty");
             else
             {
                 for (int i = 0; i <= top; i++)
                 {
-                    Console.WriteLine("Item[" + (i + 1) + "]: " + ele[i]);
+                    Console.WriteLine("Item[" + (i + 1) + "]: " + Siddiq[i]);
                 }
             }
         }
-    }
+        static void Main(string[] args)
+        {
+            Stack s = new Stack(41);
+            while (true)
+            {
+                Console.WriteLine();
+                Console.WriteLine("\n***Stack Menu***\n");
+                Console.WriteLine("1. Push Element.");
+                Console.WriteLine("2. Pop Element");
+                Console.WriteLine("3. Display");
+                Console.WriteLine("4. Exit");
+                Console.WriteLine("\nEnter your choice");
+                string sInput = Console.ReadLine();
+                char ch = Convert.ToChar(sInput == "" ? "0" : sInput);
+                switch (ch)
+                {
+                    case '1':
+                        Console.Write("\nEnter a Number of Letter: ");
+                        string letter;
+                        int num;
+                        s.Push(ch);
+                        Console.ReadLine();
+                        break;
+                    case '2':
+                        if (s.empty())
+                        {
+                            Console.WriteLine("\nStack Empty");
+                            break;
+                        }
+                        s.Pop();
+                        break;
+                    case '3':
+                        s.Display();
+                        break;
+                    case '4':
+                        return;
+                    default:
+                        Console.WriteLine("\nInvalid Choice");
+                        break;
+                }
+            }
+        }
+    }       
 }
