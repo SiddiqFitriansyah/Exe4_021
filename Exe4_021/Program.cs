a2549c264004d2117664a255e941c5178774fed6
SiddiqFitriansyah <siddiqfitriansyah14@gmail.com>
2022-12-09 15:42:50 +0700

    Add project files.

---
+++ Exe4_021/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Exe4_021
+{
+    class Siddiq
+    {
+        public int info;
+        public Siddiq next;
+        public Siddiq(int i, Siddiq S)
+        {
+            info = i;
+            next = null;
+        }
+    }
+}
